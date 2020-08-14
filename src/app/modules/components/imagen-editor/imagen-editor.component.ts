a62262d6c64bf1e762ee70a10506f5be43d7f678
santiago.yacelga <esyacelga@hotmail.com>
2020-08-14 18:08:58 -0500

    Validación de directorio

--- src/app/modules/components/imagen-editor/imagen-editor.component.ts
+++ src/app/modules/components/imagen-editor/imagen-editor.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 import {ImageGeneratorService} from '../image-generator/image-generator.service';
+import {Util} from '../../system/generic/classes/util';
+import {COLOR_TOAST_WARNING} from '../../system/generic/classes/constant';
 
 @Component({
     selector: 'app-imagen-editor',
@@ -12,21 +14,25 @@ export class ImagenEditorComponent implements OnInit, OnDestroy {
     @Input() public idImagen = '';
 
 
-    constructor(private camera: Camera, public  svrImagen: ImageGeneratorService) {
+    constructor(private camera: Camera, public  svrImagen: ImageGeneratorService, private util: Util) {
     }
 
     ngOnInit() {
     }
 
-
     ngOnDestroy() {
         this.directorio = '';
         this.idImagen = '';
+        // Esta variable quita la imagen guardada en memoria
         this.svrImagen.imagenObtenida = null;
     }
 
-
     public seleccionarPorLibreria() {
+        if (this.directorio) {
+            this.util.presentToast('Debe ingresar un directorio valido', COLOR_TOAST_WARNING);
+            return;
+        }
+
         const options: CameraOptions = {
             quality: 60,
             destinationType: this.camera.DestinationType.FILE_URI,
@@ -39,6 +45,10 @@ export class ImagenEditorComponent implements OnInit, OnDestroy {
     }
 
     public seleccionarPoCamara() {
+        if (this.directorio) {
+            this.util.presentToast('Debe ingresar un directorio valido', COLOR_TOAST_WARNING);
+            return;
+        }
         const options: CameraOptions = {
             quality: 60,
             destinationType: this.camera.DestinationType.FILE_URI,
